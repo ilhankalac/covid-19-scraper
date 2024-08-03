89a189eee9a4c82c5113a6eda445dcd117a64a06
ilhankalac <ilhan.kalac@codeus.me>
2024-08-03 16:25:38 +0200

    Code refactor with chatGPT

--- scrape_data.js
+++ scrape_data.js
@@ -2,106 +2,90 @@ const puppeteer = require('puppeteer-extra');
 const pluginStealth = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(pluginStealth());
 const environment = require('./environment.js');
+const firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/firestore");
+require("firebase/database");
 
-async function scrapeDataRozaje(){
-    (async () => {
-      scraperForRozaje(await latestLink());  
-    })();
+firebase.initializeApp(environment.firebaseConfig);
+
+async function scrapeDataRozaje() {
+  try {
+    const latestUrl = await getLatestLink();
+    if (latestUrl) {
+      const data = await scrapeRozajeData(latestUrl);
+      if (data) {
+        await insertDataToFirebase(data);
+        process.exit(1);
+      }
+    }
+  } catch (err) {
+    console.error('Error in scrapeDataRozaje:', err.message);
+  }
 }
 
-async function latestLink(){
-  
-  const browser = await puppeteer.launch();   
-  
+async function getLatestLink() {
+  const browser = await puppeteer.launch();
   try {
+    const page = await browser.newPage();
+    await page.goto('https://www.ijzcg.me/', { timeout: 0, waitUntil: 'networkidle0' });
 
-    const mainPage = await browser.newPage();
-    await mainPage.goto('https://www.ijzcg.me/',{timeout: 0, waitUntil: 'networkidle0'});
-
-    //SCRAPING ALL THE TEXT FROM THE PAGE
-    const hrefs = await mainPage.$$eval('a', as => as.map(a => a.href));
-
-    //GETTING THE LATEST LINK WITH PRESJEK SUBSTRING WITHIN IT
-    return await hrefs.filter((data) =>  JSON.stringify(data).toLowerCase().indexOf("presjek".toLowerCase()) !== -1)[0];
-    
-
-  } catch (err) { 
-    console.error(err.message);
+    const hrefs = await page.$$eval('a', as => as.map(a => a.href));
+    return hrefs.find(href => href.toLowerCase().includes('presjek'));
+  } catch (err) {
+    console.error('Error in getLatestLink:', err.message);
   } finally {
     await browser.close();
   }
 }
 
-async function scraperForRozaje(url){
-
-  const browser = await puppeteer.launch();   
+async function scrapeRozajeData(url) {
+  const browser = await puppeteer.launch();
   try {
     const page = await browser.newPage();
-    await page.goto(url,{timeout: 0, waitUntil: 'networkidle0'});
-    //SCRAPING ALL THE TEXT FROM THE PAGE
-    const allText = await page.$eval('*', el => el.innerText); 
+    await page.goto(url, { timeout: 0, waitUntil: 'networkidle0' });
 
-    //SPLITIING THE TEXT INTO ARRAY WITH STARTING WORD Rožaje
-    let rozajeFinderArray = allText.split("Rožaje");
-    let dateOfPublishing = allText.split("Objavljeno:")[1].substring(0, 13);
+    const allText = await page.$eval('*', el => el.innerText);
+    const rozajeFinderArray = allText.split("Rožaje");
+    const dateOfPublishing = allText.split("Objavljeno:")[1].substring(0, 13).trim();
 
-    let arrayOfData = [];
-    for(let j = 0; j < rozajeFinderArray.length; j++){
-
-      let finalData = "Rožaje";
-
-      for(let i = 0; i < 31; i++){
-        finalData+= rozajeFinderArray[j][i]
-      }
-
-      finalData = finalData.split("\n").filter(item => item != "" && item !="\t");
-      arrayOfData.push(finalData);
-    
+    const currentData = extractRozajeData(rozajeFinderArray);
+    if (currentData) {
+      return {
+        Datum: dateOfPublishing,
+        Grad: currentData[0],
+        Aktivni: currentData[1],
+        Oporavljeni: 'Nepoznato',
+        Umrli: 'Nepoznato'
+      };
     }
-
-    let currentData = arrayOfData[arrayOfData.length - 1];
-
-    let objectData = {
-                      "Datum": dateOfPublishing.toString().trim(), 
-                      "Grad": currentData[0], 
-                      "Aktivni": currentData[1], 
-                      "Oporavljeni": 'Nepoznato',
-                      "Umrli": 'Nepoznato'
-                    };
- 
-    insertTheDataToFirebase(objectData);
-
-    } catch (err) { 
-      console.error(err.message);
-    } finally {
-      await browser.close();
-    }
+  } catch (err) {
+    console.error('Error in scrapeRozajeData:', err.message);
+  } finally {
+    await browser.close();
+  }
 }
 
-var firebase = require("firebase/app");
-require("firebase/auth");
-require("firebase/firestore");
-
-var firebaseConfig = environment.firebaseConfig;
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-var firebase = require("firebase");
-
-
-function insertTheDataToFirebase(data) {
-  firebase.database().ref('dailyStatistics').push({
-    activeCases: data.Aktivni,
-    recovered: data.Oporavljeni,
-    deaths: data.Umrli,
-    date: data.Datum,
-  })
-.then(function onSuccess(res) {
-  process.exit(1);
-})
+function extractRozajeData(rozajeFinderArray) {
+  const latestEntry = rozajeFinderArray[rozajeFinderArray.length - 1];
+  let finalData = "Rožaje";
+  for (let i = 0; i < 31; i++) {
+    finalData += latestEntry[i];
+  }
+  return finalData.split("\n").filter(item => item !== "" && item !== "\t");
 }
 
+async function insertDataToFirebase(data) {
+  try {
+    await firebase.database().ref('dailyStatistics').push({
+      activeCases: data.Aktivni,
+      recovered: data.Oporavljeni,
+      deaths: data.Umrli,
+      date: data.Datum,
+    });
+  } catch (err) {
+    console.error('Error in insertDataToFirebase:', err.message);
+  }
+}
 
-scrappeDataRozaje();
-
+scrapeDataRozaje();
